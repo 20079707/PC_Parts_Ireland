ea967d0d02ca6095accb710171de5f0ea2c784a7
John Hogan <46961201+20079707@users.noreply.github.com>
2021-03-04 13:56:48 +0000

    Nav Bar and Recycler View added

--- app/src/main/java/ie/wit/pcpartsireland/adapters/Adapter.kt
+++ app/src/main/java/ie/wit/pcpartsireland/adapters/Adapter.kt
@@ -1,4 +1,39 @@
 package ie.wit.pcpartsireland.adapters
 
-class Adapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import ie.wit.pcpartsireland.R
+import ie.wit.pcpartsireland.models.Model
+import kotlinx.android.synthetic.main.list_cards.view.*
+
+class Adapter constructor(private var adverts: List<Model>)
+    : RecyclerView.Adapter<Adapter.MainHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
+        return MainHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.list_cards,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: MainHolder, position: Int) {
+        val part = adverts[holder.adapterPosition]
+        holder.bind(part)
+    }
+
+    override fun getItemCount(): Int = adverts.size
+
+    class MainHolder constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bind(donation: Model) {
+            itemView.name.text = donation.name
+            itemView.price.text = donation.price.toString()
+            itemView.imageIcon.setImageResource(R.mipmap.ic_launcher_round)
+        }
+    }
 }
--- app/src/main/java/ie/wit/pcpartsireland/models/Model.kt
+++ app/src/main/java/ie/wit/pcpartsireland/models/Model.kt
@@ -1,4 +1,9 @@
 package ie.wit.pcpartsireland.models
 
-class Model {
-}
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Model(var id: Long = 0,
+                         val price: Int = 0,
+                         val name: String = "N/A") : Parcelable
