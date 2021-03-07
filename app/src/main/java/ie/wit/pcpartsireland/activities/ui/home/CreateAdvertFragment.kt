7a6f767faf82831aae6fe424262516d6848123a1
John Hogan <46961201+20079707@users.noreply.github.com>
2021-03-07 23:33:21 +0000

    Create Advert UI Completed

--- app/src/main/java/ie/wit/pcpartsireland/activities/ui/home/CreateAdvertFragment.kt
+++ app/src/main/java/ie/wit/pcpartsireland/activities/ui/home/CreateAdvertFragment.kt
@@ -7,6 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import ie.wit.pcpartsireland.R
+import it.sephiroth.android.library.numberpicker.doOnProgressChanged
+import it.sephiroth.android.library.numberpicker.doOnStartTrackingTouch
+import it.sephiroth.android.library.numberpicker.doOnStopTrackingTouch
+import kotlinx.android.synthetic.main.fragment_create_advert.*
+import kotlinx.android.synthetic.main.fragment_create_advert.view.*
+import kotlinx.android.synthetic.main.list_cards.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,10 +26,42 @@ class CreateAdvertFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        
-        return inflater.inflate(R.layout.fragment_create_advert, container, false)
-    }
 
+
+
+        val root = inflater.inflate(R.layout.fragment_create_advert, container, false)
+
+
+
+        val spinner = root.findViewById<Spinner>(R.id.mySpinner)
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(requireContext(), R.array.categories, R.layout.spinner_item_selected).also { adapter ->
+
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(R.layout.spinner_dropdown_item)
+
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+            spinner.onItemSelectedListener = object :
+
+                AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View,
+                    position: Int,
+                    id: Long
+                ) {
+
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+
+                }
+            }
+            return root
+        }
+    }
     companion object {
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
